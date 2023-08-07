34a7f83fe3cf9228a2b3fcf75fcfa5237ff05c63
BANGBANGROX <lakshya0809bang@gmail.com>
2023-08-07 20:55:04 +0530

    added one more func

--- test.py
+++ test.py
@@ -5,3 +5,12 @@ def find_sum(nums: list) -> int:
         result += num
 
     return num
+
+
+def find_sqaure_sum(nums: list) -> int:
+    result = 0
+
+    for num in nums:
+        result += num * num
+
+    return result
